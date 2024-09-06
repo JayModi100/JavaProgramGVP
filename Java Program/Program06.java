0eb1f20b1c7a8752bdcf2c95c8c086c7e71dd3e2
JayModi100 <jayheruwala100@gmail.com>
2024-09-06 15:30:14 +0530

    Area Of Circel

--- Java Program/Program06.java
+++ Java Program/Program06.java
@@ -1,7 +1,18 @@
+// Create a class “Circle” with 1) private variables x, y, and r
+// Here x and y are coordinates of center of circle and r is a radius 
+// 2) Define all possible constructors.
+// 3) Define a method – public double area() which is calculate and return area of a 
+// circle.
+// 4) Define a method – public double circumference() which is calculate and return 
+// perimeter of a circle.
+// 5) Define a method – public double diameter() which is calculate and return 
+// diameter of a circle
+
 
 class Circel{
     private double x;
-    private double y, r;
+    private double y;
+    private double r;
 
     Circel(){
         this.x = 2.5;
@@ -31,6 +42,15 @@ class Circel{
     public double diameter(){
         return 2*r;
     }
+
+    public void printMethod(){
+        System.out.println("\nCircel Details - ");
+        System.out.println("Coordinates of center : (" + x +","+y+")");
+        System.out.println("Redius : " + r);
+        System.out.println("Area: " + area());
+        System.out.println("Circumference: " + circumference());
+        System.out.println("Diameter: " + diameter());
+    }
 }
 
 public class Program06 {
@@ -39,20 +59,10 @@ public class Program06 {
         Circel c2 = new Circel(3.5,3.6,2.5);
         Circel c3 = new Circel(c2);
 
-        System.out.println("Circel c1 - ");
-        System.out.println("Area: " + c1.area());
-        System.out.println("Circumference: " + c1.circumference());
-        System.out.println("Diameter: " + c1.diameter());
 
-        System.out.println("Circel c2 - ");
-        System.out.println("Area : "+c2.area());
-        System.out.println("Ciccumfrence: "+c2.circumference());
-        System.out.println("Diameter: "+ c2.diameter());
-
-        System.out.println("Circel c3 - ");
-        System.out.println("Area : "+c3.area());
-        System.out.println("Ciccumfrence: "+c3.circumference());
-        System.out.println("Diameter: "+ c3.diameter());
+        c1.printMethod();
+        c2.printMethod();
+        c3.printMethod();
         
     }
 }
