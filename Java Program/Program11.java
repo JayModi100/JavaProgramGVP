76a95d9d66af92c98db56341fab0158d20151741
JayModi100 <jayheruwala100@gmail.com>
2024-09-07 18:26:29 +0000

    Updated Program

---
+++ Java Program/Program11.java
@@ -0,0 +1,25 @@
+
+class NegativeValueException extends Exception {
+    NegativeValueException(String s) {
+        super(s);
+    }
+}
+
+public class Program11 {
+
+    public static void main(String[] args) {
+        // int number[] = new int[args.length];
+        int number[] = {10,20,30,40,50,-10,20};
+        try {
+
+            for (int i = 0; i < number.length; i++) {
+                // number[i] = Integer.parseInt(args[i]);
+                if (number[i] < 0) {
+                    throw new NegativeValueException(number[i] + " is negative");
+                }
+            }
+        } catch (Exception e) {
+           System.out.println("Error : "+ e.getMessage());
+        }
+    }
+}
