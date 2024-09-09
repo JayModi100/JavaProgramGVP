3ea5503542ff3d6ebb85284a3420bd2bb8a6d310
JayModi100 <jayheruwala100@gmail.com>
2024-09-09 16:03:29 +0000

    Change

--- Java Program/Program11.java
+++ Java Program/Program11.java
@@ -9,7 +9,6 @@ public class Program11 {
 
     public static void main(String[] args) {
         int number[] = new int[args.length];
-        // int number[] = {10,20,30,40,50,-10,20};
         try {
 
             for (int i = 0; i < number.length; i++) {
