da7754fa09a48d6d33dadbbb52e82730497cdfda
JayModi100 <jayheruwala100@gmail.com>
2024-09-20 14:54:07 +0530

    Update Program06.java

--- Java Program/Program06.java
+++ Java Program/Program06.java
@@ -1,69 +1,118 @@
-// Create a class “Circle” with 1) private variables x, y, and r
-// Here x and y are coordinates of center of circle and r is a radius 
-// 2) Define all possible constructors.
-// 3) Define a method – public double area() which is calculate and return area of a 
-// circle.
-// 4) Define a method – public double circumference() which is calculate and return 
-// perimeter of a circle.
-// 5) Define a method – public double diameter() which is calculate and return 
-// diameter of a circle
+/* Create a class “Circle” with 
+1) private variables x, y, and r
+Here x and y are coordinates of center of circle and r is a radius 
+2) Define all possible constructors.
+3) Define a method – public double area() which is calculate and return area of a 
+circle.
+4) Define a method – public double circumference() which is calculate and return 
+perimeter of a circle.
+5) Define a method – public double diameter() which is calculate and return 
+diameter of a circle */
 
-import java.lang.Math;
+import java.util.Scanner;
 
-class Circel{
+class Circle {
     private double x;
     private double y;
     private double r;
 
-    Circel(){
+    Circle() {
         this.x = 2.5;
-        this.y = 2.6;
-        this.r = 2.7;
+        this.y = 3.5;
+        this.r = 2.3;
     }
-    Circel(double x,double y,double r){
+
+    Circle(double x, double y, double r) {
         this.x = x;
         this.y = y;
         this.r = r;
     }
 
-    Circel(Circel copy){
-        this.x = copy.x;
-        this.y = copy.y;
-        this.r = copy.r;
+    public double area() {
+        double areaOfCircle = Math.PI * this.r * this.r;
+        return areaOfCircle;
     }
 
-    public double area(){
-        return Math.PI * r *r;
+    public double circumference() {
+        double circumferenceOfCircle = 2 * Math.PI * this.r;
+        return circumferenceOfCircle;
     }
 
-    public double circumference(){
-        return 2 * Math.PI * r;
+    public double diameter() {
+        double diameterOfCircle = 2 * this.r;
+        return diameterOfCircle;
     }
 
-    public double diameter(){
-        return 2*r;
+    double getX(){
+        return this.x;
     }
 
-    public void printMethod(){
-        System.out.println("\nCircel Details - ");
-        System.out.println("Coordinates of center : (" + x +","+y+")");
-        System.out.println("Redius : " + r);
-        System.out.println("Area: " + area());
-        System.out.println("Circumference: " + circumference());
-        System.out.println("Diameter: " + diameter());
+    double getY(){
+        return this.y;
     }
+
+    double getR(){
+        return this.r;
+    }
+
 }
 
-public class Program06 {
+public class Progrma06 {
     public static void main(String[] args) {
-        Circel c1 = new Circel();
-        Circel c2 = new Circel(3.5,3.6,4);
-        Circel c3 = new Circel(c2);
 
+        // Scanner Class
+            Scanner takeInput = new Scanner(System.in);
 
-        c1.printMethod();
-        c2.printMethod();
-        c3.printMethod();
-        
+        System.out.println("Find the Area of Circle,Circumfernce,Diameter");
+        while (true) {
+
+            try {
+                int choice;
+                System.out.println("Enter 1 for Default Value : ");
+                System.out.println("Enter 2 for User Input : ");
+                System.out.print("Enter Choice : ");
+                choice = takeInput.nextInt();
+                switch (choice) {
+                    case 1:
+                        Circle c1 = new Circle();
+                        System.out.println("Value of X coordinates : "+ c1.getX());
+                        System.out.println("Value of Y coordinates : "+c1.getY());
+                        System.out.println("Value of Radius : "+c1.getR());
+                        System.out.println("Area of Circle : " + c1.area());
+                        System.out.println("Circumfernce of Circle : " + c1.circumference());
+                        System.out.println("Diameter of Circle : " + c1.diameter());
+                        break;
+
+                    case 2:
+                        try {
+                            double x, y, r;
+                            System.out.print("Enter the X coordinate :  ");
+                            x = takeInput.nextDouble();
+
+                            System.out.print("Enter the Y coordinate : ");
+                            y = takeInput.nextDouble();
+
+                            System.out.print("Enter the Radius : ");
+                            r = takeInput.nextDouble();
+
+                            Circle c2 = new Circle(x, y, r);
+                            System.out.println("Value of X coordinates : "+ c2.getX());
+                            System.out.println("Value of Y coordinates : "+c2.getY());
+                            System.out.println("Value of Radius : "+c2.getR());
+                            System.out.println("Area of Circle : " + c2.area());
+                            System.out.println("Circumfernce of Circle : " + c2.circumference());
+                            System.out.println("Diameter of Circle : " + c2.diameter());
+                        } catch (Exception e) {
+                            System.out.println("Error : " + e.getMessage());
+                        }
+
+                    default:
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Error : " + e.getMessage());
+            }
+        }
+
     }
 }
